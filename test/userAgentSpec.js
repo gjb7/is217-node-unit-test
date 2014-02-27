ab3042d4993924252e479a7574caa9181d1a5d74
Grant J. Butler <butler.grant.j@gmail.com>
2014-02-27 17:39:19 -0500

    Added tests for the assignment/

--- test/userAgentSpec.js
+++ test/userAgentSpec.js
@@ -158,4 +158,58 @@ describe('UserAgent', function() {
 			expect(result).to.deep.equal(expected);
 		});
 	});
+	
+	describe("#isMobile", function() {
+		it("should detect an iPhone user agent string", function() {
+			var userAgentString = "Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53";
+			
+			var result = userAgent.isMobile(userAgentString);
+
+			expect(result).to.be.true;
+		});
+		
+		it("should detect an Android user agent string", function() {
+			var userAgentString = "Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19";
+			
+			var result = userAgent.isMobile(userAgentString);
+
+			expect(result).to.be.true;
+		});
+		
+		it("should detect a Windows Phone user agent string", function() {
+			var userAgentString = "Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0; Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 920)";
+			
+			var result = userAgent.isMobile(userAgentString);
+
+			expect(result).to.equal(true);
+		});
+	})
+	
+	describe("#isTablet", function() {
+		it("should detect an iPad user agent string", function() {
+			var userAgentString = "Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0; Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 920)";
+			
+			var result = userAgent.isTablet(userAgentString);
+			
+			expect(result).to.be.true;
+		});
+	});
+	
+	describe("#isDesktop", function() {
+		it("should detect a desktop user agent string", function() {
+			var userAgentString = "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.0.7) Gecko Kazehakase/0.5.6";
+			
+			var result = userAgent.isDesktop(userAgentString);
+			
+			expect(result).to.be.false;
+		});
+		
+		it("should not detect a mobile user agent string", function() {
+			var userAgentString = "Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0; Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 920)";
+			
+			var result = userAgent.isMobile(userAgentString);
+
+			expect(result).to.not.be.true;
+		});
+	})
 });
